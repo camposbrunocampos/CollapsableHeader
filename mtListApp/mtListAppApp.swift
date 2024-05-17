6bb35ffe4cdd6df67df4ce876274f03b2806315b
Bruno <camposbrunocampos@gmail.com>
2024-05-17 14:21:42 -0300

    Initial Commit

---
+++ mtListApp/mtListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  mtListAppApp.swift
+//  mtListApp
+//
+//  Created by Bruno Campos on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct mtListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
